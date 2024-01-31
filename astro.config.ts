1618a7facc900c2e79b041af54bc1128bcb6e369
piapot <piapot.hou@outlook.com>
2024-01-31 17:22:25 +0800

    chore: migrate unocss to tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,31 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
+import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 import solid from '@astrojs/solid-js'
-import unocss from 'unocss/astro'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), unocss()],
+  site: 'https://piapot.me',
   output: 'static',
   adapter: vercel({
+    imageService: true,
     webAnalytics: { enabled: true },
   }),
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'one-dark-pro' },
+      remarkPlugins: [remarkToc],
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false,
+    }),
+    tailwind(),
+    icon(),
+    solid({ include: ['**/solid/*'] }),
+    sitemap(),
+  ],
 })
